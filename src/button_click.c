aaf296cc3f94523fecb427a47e9594b05ddc347e
Yoctopuce Sarl <dev@yoctopuce.com>
2015-06-17 00:25:36 +0200

    add pebble time layout

--- src/button_click.c
+++ src/button_click.c
@@ -1,25 +1,50 @@
 #include <pebble.h>
 
 #define TEST_MESSAGE_STR 1  
+#define TEST_STATE       2  
   
-#define KEY_BUTTON    0
-#define BUTTON_UP     0
-#define BUTTON_SELECT 1
-#define BUTTON_DOWN   2
+  
+#define KEY_CMD       0
+#define CMD_CLEAN     0
+#define CMD_SPOT      1
+#define CMD_DOCK      2
+#define CMD_UNKNOWN   3
+  
+  
+// misc size defintion
+  
+#define ROOMBA_SIZE 66
+#define CROSS_SIZE 75
+#define CLEAN_ICON_SIZE 15
+#define SPOT_ICON_SIZE 15
+#define HOME_ICON_SIZE 15
+  
+  
   
 static Window *window;
-static TextLayer *text_layer;
 static TextLayer *text_layer_message;
 static ActionBarLayer *action_bar;
 
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+static BitmapLayer *s_cross_layer;
+static GBitmap *s_cross_bitmap;
 
-// The implementation of my_next_click_handler and my_previous_click_handler
-// is omitted for the sake of brevity. See the Clicks reference docs.
+static GBitmap *s_clean_bitmap, *s_spot_bitmap, *s_home_bitmap;
 
 
 static char conditions_buffer[32];
+
+  
+
+static void refreshMessage(const char* msg)
+{
+  snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", msg);
+  text_layer_set_text(text_layer_message, conditions_buffer);
+}
+  
+
+
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Read first item
@@ -30,8 +55,8 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     // Which key was received?
     switch(t->key) {
     case TEST_MESSAGE_STR:
-      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-      text_layer_set_text(text_layer_message, conditions_buffer);
+      refreshMessage(t->value->cstring);
+      break;
       break;
     default:
       APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
@@ -46,9 +71,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
 static void send(int key, int message) {
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
-
   dict_write_int(iter, key, &message, sizeof(int), true);
-
   app_message_outbox_send();
 }
 
@@ -64,20 +87,17 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
-  send(KEY_BUTTON, BUTTON_SELECT);
+  send(KEY_CMD, CMD_CLEAN);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-  send(KEY_BUTTON, BUTTON_UP);
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send(KEY_CMD, CMD_SPOT);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-  send(KEY_BUTTON, BUTTON_DOWN);
+  send(KEY_CMD, CMD_DOCK);
 }
 
 static void click_config_provider(void *context) {
@@ -88,34 +108,40 @@ static void click_config_provider(void *context) {
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-#ifdef PBL_COLOR
-  window_set_background_color(window, GColorPictonBlue);
-#else
-  window_set_background_color(window, GColorWhite);
-#endif
+  
+  
   GRect bounds = layer_get_bounds(window_layer);
   // Create roomba icon GBitmap
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_PEEBLE_IMAGE);
+#ifdef PBL_COLOR
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ROOMBA_COLOR);
+#else
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ROOMBA_BW);
+#endif
   // create roomba icon layer
-  s_background_layer = bitmap_layer_create(GRect(0, 35, 114,114));
+  s_background_layer = bitmap_layer_create(GRect((bounds.size.w - ACTION_BAR_WIDTH - ROOMBA_SIZE)/2, (bounds.size.h - ROOMBA_SIZE - 20), ROOMBA_SIZE,ROOMBA_SIZE));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);  
 #ifdef PBL_SDK_3
   bitmap_layer_set_compositing_mode(s_background_layer,GCompOpSet);
 #endif
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 
-  // create top message
-  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w - ACTION_BAR_WIDTH, 30 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-  // create bottin
-  text_layer_message = text_layer_create((GRect) { .origin = { 0, bounds.size.h - 30 }, .size = { bounds.size.w - ACTION_BAR_WIDTH, 30 } });
-  text_layer_set_text(text_layer_message, "...");
+ // Create roomba icon GBitmap
+  s_cross_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CROSS);
+  // create roomba icon layer
+  s_cross_layer = bitmap_layer_create(GRect((bounds.size.w - ACTION_BAR_WIDTH - CROSS_SIZE)/2, (bounds.size.h - CROSS_SIZE - 20), CROSS_SIZE,CROSS_SIZE));
+  bitmap_layer_set_bitmap(s_cross_layer, s_cross_bitmap);  
+#ifdef PBL_SDK_3
+  bitmap_layer_set_compositing_mode(s_cross_layer,GCompOpSet);
+#endif
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_cross_layer));
+
+
+  
+
+  // create message layer
+  text_layer_message = text_layer_create((GRect) { .origin = { 0, 10 }, .size = { bounds.size.w -  ACTION_BAR_WIDTH , 30 } });
   text_layer_set_background_color(text_layer_message,GColorClear);
-  text_layer_set_font(text_layer_message, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_font(text_layer_message, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   text_layer_set_text_alignment(text_layer_message, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer_message));
 
@@ -127,27 +153,46 @@ static void window_load(Window *window) {
   action_bar_layer_set_click_config_provider(action_bar,
                                              click_config_provider);
 
+   s_clean_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CLEAN_ICON);
+   s_spot_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SPOT_ICON);
+   s_home_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DOCK_ICON);
+
+  
   // Set the icons:
   // The loading the icons is omitted for brevity... See HeapBitmap.
-  //action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_UP, &my_icon_previous, true);
-  //action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_DOWN, &my_icon_next, true);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, s_clean_bitmap);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT, s_spot_bitmap);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, s_home_bitmap);
 
 
   
-  
+  refreshMessage("...");
+
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
-  text_layer_destroy(text_layer_message);
   action_bar_layer_destroy(action_bar);
+  text_layer_destroy(text_layer_message);
+  bitmap_layer_destroy(s_cross_layer);
+  gbitmap_destroy(s_cross_bitmap);
   bitmap_layer_destroy(s_background_layer);
   gbitmap_destroy(s_background_bitmap);
+  
 }
 
 static void init(void) {
   window = window_create();
+#ifndef PBL_SDK_3
+    window_set_fullscreen(window, true);
+#endif
+#ifdef PBL_COLOR
+  window_set_background_color(window, GColorPictonBlue);
+#else
+  window_set_background_color(window, GColorWhite);
+#endif
+
   window_set_click_config_provider(window, click_config_provider);
+
   window_set_window_handlers(window, (WindowHandlers) {
 	.load = window_load,
     .unload = window_unload,
