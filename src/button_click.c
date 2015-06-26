8a592e16e193d8ea43882a5f2429dc18c4299919
Yoctopuce Sarl <dev@yoctopuce.com>
2015-06-26 09:56:24 +0200

    Refresh information each 4 seconds

--- src/button_click.c
+++ src/button_click.c
@@ -43,6 +43,7 @@ static GBitmap *s_spot_bitmap;
 static GBitmap *s_dock_bitmap;
 
 static char message_buffer[32];
+static int s_uptime = 0;
 
 static void refreshMessage(const char* msg)
 {
@@ -133,14 +134,23 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+   // Increment s_uptime
+  s_uptime++;
+  if ( (s_uptime & 0x3) == 0){
+    send(KEY_CMD, CMD_GET_STATE);
+  }
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   
   
   GRect bounds = layer_get_bounds(window_layer);
-  // Create roomba icon GBitmap
+  // Create roomba GBitmap
   s_roomba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ROOMBA_FULL);
-  // create roomba icon layer
+  // create roomba layer
   s_roomba_layer = bitmap_layer_create(GRect((bounds.size.w - ACTION_BAR_WIDTH - ROOMBA_SIZE)/2, (bounds.size.h - ROOMBA_SIZE - 5), ROOMBA_SIZE,ROOMBA_SIZE));
   bitmap_layer_set_bitmap(s_roomba_layer, s_roomba_bitmap);  
 #ifdef PBL_SDK_3
@@ -148,9 +158,9 @@ static void window_load(Window *window) {
 #endif
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_roomba_layer));
 
- // Create roomba icon GBitmap
+ // Create cross GBitmap
   s_cross_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CROSS_FULL);
-  // create roomba icon layer
+  // create cross layer
   s_cross_layer = bitmap_layer_create(GRect((bounds.size.w - ACTION_BAR_WIDTH - CROSS_SIZE)/2, (bounds.size.h - CROSS_SIZE - 5), CROSS_SIZE,CROSS_SIZE));
   bitmap_layer_set_bitmap(s_cross_layer, s_cross_bitmap);  
 #ifdef PBL_SDK_3
@@ -158,7 +168,6 @@ static void window_load(Window *window) {
 #endif
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_cross_layer));
   
-
   // create message layer
   s_text_layer_message = text_layer_create((GRect) { .origin = { 0, 10 }, .size = { bounds.size.w -  ACTION_BAR_WIDTH , 30 } });
   text_layer_set_background_color(s_text_layer_message,GColorClear);
@@ -198,7 +207,7 @@ static void init(void) {
     window_set_fullscreen(window, true);
 #endif
 #ifdef PBL_COLOR
-  window_set_background_color(window, GColorPictonBlue);
+  window_set_background_color(window, GColorLimerick);
 #else
   window_set_background_color(window, GColorWhite);
 #endif
@@ -221,6 +230,9 @@ static void init(void) {
   
   const bool animated = true;
   window_stack_push(window, animated);
+  
+  // Subscribe to TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit(void) {
